3ab1fe09d36852fd4fe60726109f9151e6ecb166
davidserr264 <davidserr264@gmail.com>
2022-05-08 09:13:33 -0400

    first commit

---
+++ myExpressApp/controllers/landing.js
@@ -0,0 +1,79 @@
+const models = require('../models')
+const confirmDelete = require('../models');
+
+exports.get_landing = (req, res, next) =>
+    res.render('landing', { title: 'Express from David with Controllers' });
+
+exports.submit_lead = (req, res, next) => {
+    return models.Lead.create({
+        email: req.body.lead_email,
+        name: req.body.lead_name,
+        id: req.body.lead_id
+    }).then(lead => {
+        res.redirect('/leads');
+    });
+}
+
+
+exports.show_leads = (req, res, next) => {
+    models.Lead.findAll().then(leads => {
+        res.render('landing', { title: 'Express from David with Controllers', leads: leads });
+    })
+}
+
+exports.show_lead = (req, res, next) => {
+    return models.Lead.findOne({
+        where: {
+            id: req.params.lead_id
+        }
+    }).then(lead => {
+        res.render('lead', { lead: lead });//passing any object to the view
+    });
+}
+
+exports.show_edit_lead = (req, res, next) => {
+    return models.Lead.findOne({
+        where: {
+            id: req.params.lead_id
+        }
+    }).then(lead => {
+        res.render('lead/edit_lead', { lead: lead });//form to edit
+    });
+}
+
+exports.edit_lead = (req, res, next) => {
+    return models.Lead.update({
+        id: parseInt(req.params.lead_id),
+        name: req.body.lead_name,
+        email: req.body.lead_email
+    }, {
+        where: {
+            id: req.params.lead_id
+        }
+    }).then(result => {
+        console.log(result);
+        res.redirect('/lead/' + parseInt(req.params.lead_id))
+        // res.redirect('/') //from http://localhost:3000/lead/13/edit to http://localhost:3000/lead/13
+    })
+}
+
+exports.delete_lead = function (req, res, next) {
+    return models.Lead.destroy({
+        where: {
+            id: req.params.lead_id
+        }
+    }).then(result => {
+        res.redirect('/leads');
+    })
+}
+
+exports.delete_lead_json = function (req, res, next) {
+
+    return models.Lead.destroy({
+        where: {
+            id: req.params.lead_id
+        }
+    }).then(result => {
+        res.send({ msg: "Success" });
+    })
+}
